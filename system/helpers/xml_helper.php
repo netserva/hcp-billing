a86257152f2c239b72edef66eb15cac30c0ba14c
Mark Constable <markc@renta.net>
2022-01-16 17:43:10 +1000

    Run risky php-cs-fixer over system/ files

--- system/helpers/xml_helper.php
+++ system/helpers/xml_helper.php
@@ -1,6 +1,8 @@
 <?php
+
+declare(strict_types=1);
 /**
- * CodeIgniter
+ * CodeIgniter.
  *
  * An open source application development framework for PHP
  *
@@ -26,18 +28,18 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
  * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
  * @license	https://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
+ *
+ * @see	https://codeigniter.com
  * @since	Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-/**
+/*
  * CodeIgniter XML Helpers
  *
  * @package		CodeIgniter
@@ -49,42 +51,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // ------------------------------------------------------------------------
 
-if ( ! function_exists('xml_convert'))
-{
-	/**
-	 * Convert Reserved XML characters to Entities
-	 *
-	 * @param	string
-	 * @param	bool
-	 * @return	string
-	 */
-	function xml_convert($str, $protect_all = FALSE)
-	{
-		$temp = '__TEMP_AMPERSANDS__';
+if (!function_exists('xml_convert')) {
+    /**
+     * Convert Reserved XML characters to Entities.
+     *
+     * @param	string
+     * @param	bool
+     * @param mixed $str
+     * @param mixed $protect_all
+     *
+     * @return string
+     */
+    function xml_convert($str, $protect_all = false)
+    {
+        $temp = '__TEMP_AMPERSANDS__';
 
-		// Replace entities to temporary markers so that
-		// ampersands won't get messed up
-		$str = preg_replace('/&#(\d+);/', $temp.'\\1;', $str);
+        // Replace entities to temporary markers so that
+        // ampersands won't get messed up
+        $str = preg_replace('/&#(\d+);/', $temp.'\\1;', $str);
 
-		if ($protect_all === TRUE)
-		{
-			$str = preg_replace('/&(\w+);/', $temp.'\\1;', $str);
-		}
+        if (true === $protect_all) {
+            $str = preg_replace('/&(\w+);/', $temp.'\\1;', $str);
+        }
 
-		$str = str_replace(
-			array('&', '<', '>', '"', "'", '-'),
-			array('&amp;', '&lt;', '&gt;', '&quot;', '&apos;', '&#45;'),
-			$str
-		);
+        $str = str_replace(
+            ['&', '<', '>', '"', "'", '-'],
+            ['&amp;', '&lt;', '&gt;', '&quot;', '&apos;', '&#45;'],
+            $str
+        );
 
-		// Decode the temp markers back to entities
-		$str = preg_replace('/'.$temp.'(\d+);/', '&#\\1;', $str);
+        // Decode the temp markers back to entities
+        $str = preg_replace('/'.$temp.'(\d+);/', '&#\\1;', $str);
 
-		if ($protect_all === TRUE)
-		{
-			return preg_replace('/'.$temp.'(\w+);/', '&\\1;', $str);
-		}
+        if (true === $protect_all) {
+            return preg_replace('/'.$temp.'(\w+);/', '&\\1;', $str);
+        }
 
-		return $str;
-	}
+        return $str;
+    }
 }
